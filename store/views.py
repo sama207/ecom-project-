a62f176d8977cb13e0ac53b883a79d98c8b60e6b
sama <samashalabi2018@gmail.com>
2024-09-02 19:03:34 +0300

    video10

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -16,6 +16,28 @@ def home(request):
 
 def about(request):
     return render(request, "about.html", {})
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, "product.html", {"product": product})
+
+
+def category(request, foo):
+    # replace hyphens with spaces
+    foo = foo.replace("-", " ")
+    # grab the category from url
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(
+            request, "category.html", {"products": products, "category": category}
+        )
+    except:
+        messages.success(
+                request, ("that category doesnt exists")
+            )
+        return redirect('home')
 
 
 def login_user(request):
@@ -42,21 +64,23 @@ def logout_user(request):
 
 
 def register_user(request):
-    form=SignUpForm()
-    if request.method=="POST":
-        form=SignUpForm(request.POST)
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username=form.cleaned_data['username']
-            password=form.cleaned_data['password1']
-            #login user
-            user=authenticate(username=username,password=password)
-            login(request,user)
-            messages.success(request,("you have been registered succefully"))
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            # login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("you have been registered succefully"))
             return redirect("home")
         else:
-            messages.success(request,("whoops! there was a problem while registering pls try again"))
-            return redirect('register')
+            messages.success(
+                request, ("whoops! there was a problem while registering pls try again")
+            )
+            return redirect("register")
 
     else:
-        return render(request, "register.html", {'form':form})
+        return render(request, "register.html", {"form": form})
