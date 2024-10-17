09cc44f57f1ee4a31bf464f7e0b653b9a3873aa2
sama <samashalabi2018@gmail.com>
2024-10-17 12:31:56 +0300

    done video 22

--- store/views.py
+++ store/views.py
@@ -4,9 +4,26 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm,UpdateUserForm
 from django import forms
 
+def update_user(request):
+    if(request.user.is_authenticated):
+        current_user=User.objects.get(id=request.user.id)
+        user_form=UpdateUserForm(request.POST or None ,instance=current_user)
+
+        if user_form.is_valid():
+            user_form.save()
+
+
+            login(request,current_user)
+            messages.success(request,"user has been updated!")
+            return redirect('home')
+        
+        return render(request,"update_user.html",{'user_form':user_form})
+    else:
+            messages.success(request,"you must be logged in")
+            return redirect("home")
 
 # Create your views here.
 def home(request):
